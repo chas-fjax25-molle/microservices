b774ae0469f3a85153e93943fd80101455181e28
Oliver Öhrström <xzereha@users.noreply.github.com>
2026-06-10 11:20:52 +0200

    Basic javadocs for the controller

--- user-service/src/main/java/com/example/user/controller/UserController.java
+++ user-service/src/main/java/com/example/user/controller/UserController.java
@@ -31,27 +31,59 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * Validate that the login credentials are valid and return the user details if
+     * they are.
+     * 
+     * @param request Login credentials
+     * @return The user details if the login credentials are valid
+     */
     @PostMapping("/validate")
     public ResponseEntity<UserResponseDTO> validateLogin(@RequestBody @Validated LoginRequestDTO request) {
         return ResponseEntity.ok(userService.validateUser(request));
     }
 
+    /**
+     * Get the user details for a given ID.
+     * 
+     * @param id The ID of the user to get
+     * @return The user details for the given ID if it exists
+     */
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> getUser(@PathVariable @NotNull @NonNull UUID id) {
         return ResponseEntity.ok(userService.getUserById(id));
     }
 
+    /**
+     * Register a new user.
+     * 
+     * @param entity The user details to register
+     * @return The user details for the newly registered user
+     */
     @PostMapping("/register")
     public ResponseEntity<UserResponseDTO> registerUser(@RequestBody @Validated UserRegisterDTO entity) {
         return ResponseEntity.ok(userService.registerUser(entity));
     }
 
+    /**
+     * Update the user details for a given ID.
+     * 
+     * @param id     The ID of the user to update
+     * @param entity Structure containing the updated user details
+     * @return The updated user details for the given ID if it exists
+     */
     @PatchMapping("/{id}")
     public ResponseEntity<UserResponseDTO> updateUser(@PathVariable @NotNull @NonNull UUID id,
             @RequestBody @Validated UserUpdateDTO entity) {
         return ResponseEntity.ok(userService.updateUser(id, entity));
     }
 
+    /**
+     * Delete the user with the specified ID.
+     * 
+     * @param id The ID of the user to delete
+     * @return A response indicating that the user has been deleted successfully
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable @NotNull @NonNull UUID id) {
         userService.deleteUser(id);
