25f808d5cfc0fdd122f07ec7b542559bad17c905
Mario <zalac.mario1@gmail.com>
2026-06-10 13:57:32 +0200

    JPA needes table name to inser properly.

--- user-service/src/main/java/com/example/user/model/User.java
+++ user-service/src/main/java/com/example/user/model/User.java
@@ -7,12 +7,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
