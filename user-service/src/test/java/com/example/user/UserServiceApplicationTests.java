cf13e422634ed96176a3bd17019414e6eae72c92
Mario <zalac.mario1@gmail.com>
2026-06-12 11:40:42 +0200

    Changed versions for spring boot, and java to get spring cloud BOM working. That changed also som dependencies

--- user-service/src/test/java/com/example/user/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/example/user/UserServiceApplicationTests.java
@@ -1,8 +1,8 @@
 package com.example.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
